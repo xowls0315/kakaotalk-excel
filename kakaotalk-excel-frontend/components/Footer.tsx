01ef114222e24e21bce5aaeea931ad59425161e6
t.jin_01 <155832601+xowls0315@users.noreply.github.com>
2025-12-30 09:45:29 +0900

    Revert "Frontend:create"

--- kakaotalk-excel-frontend/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="border-t bg-white py-8">
-      <div className="container mx-auto px-4 text-center text-sm text-gray-600">
-        <p>© 2025 Talk to Excel. All rights reserved.</p>
-      </div>
-    </footer>
-  );
-}
